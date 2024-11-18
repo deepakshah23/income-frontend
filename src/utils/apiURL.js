77581b3e87f1eb1a5b423017c26d163677c1c5b6
deepakshah <deepakshah5348@gmail.com>
2024-11-18 20:48:23 +0530

    first commit

---
+++ src/utils/apiURL.js
@@ -0,0 +1,3 @@
+export const API_URL_USER = "https://incomebackend.onrender.com/api/v1/users";
+export const API_URL_ACCOUNT = "https://incomebackend.onrender.com/api/v1/accounts";
+export const API_URL_TRANSACTION = "https://incomebackend.onrender.com/api/v1/transactions/";
